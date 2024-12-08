e797cb96c1c33651a5919ac8ade1920f825d4618
hengnarak <hengnarak0711@gmail.com>
2024-12-08 20:27:18 +0700

    Just done some menu

---
+++ Code/logger.py
@@ -0,0 +1,24 @@
+while True:
+    try :
+        print("\nMenu:")
+        print("1. Login")
+        print("2. View Logs")
+        print("3. Change Password")
+        print("4. Reset Password")
+        print("5. Exit")
+        choice=int(input("Enter the number:"))
+        if choice == 1:
+            print("Lol")
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            pass
+        elif choice == 4:
+            pass
+        elif choice == 5:
+            pass
+            break
+        else :
+            print("Invalid choice. Please try again.")
+    except :
+        print(f"An unexpected error occurred ")
